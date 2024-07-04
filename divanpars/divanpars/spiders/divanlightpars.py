8cfe81e995047cf98fc423b3a066a25bb3b9869d
rsi2024 <164152459+rsi2024@users.noreply.github.com>
2024-07-04 23:33:59 +0300

    (csv)

--- divanpars/divanpars/spiders/divanlightpars.py
+++ divanpars/divanpars/spiders/divanlightpars.py
@@ -1,17 +1,33 @@
-import scrapy
+import time
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+driver = webdriver.Firefox()
+driver.get("https://divan.ru/category/svet")
+time.sleep(5)
 
-class DivanlightparsSpider(scrapy.Spider):
-    name = "divanlightpars"
-    allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://divan.ru/category/svet"]
+svets = driver.find_elements(By.CLASS_NAME, '_Ud0k')
+print(f"Найдено элементов: {len(svets)}")
 
-    def parse(self, response):
-        svets = response.css('div._Ud0k')
-        for svet in svets:
-            yield {
-                'name': svet.css('div.wYUX2 span::text').get(),
-                'price': svet.css('div.pY3d2 span::text').get(),
-                'url': svet.css('a').attrib['href']
-            }
+parsed_data = []
+
+for svet in svets:
+    try:
+        name = svet.find_element(By.CSS_SELECTOR, 'div.wYUX2 span').text
+        price = svet.find_element(By.CSS_SELECTOR, 'div.pY3d2 span').text
+        link = svet.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+        parsed_data.append([name, price, link])
+        print(f"Название: {name}, Цена: {price}, Ссылка: {link}")
+    except Exception as e:
+        print(f"Произошла ошибка при парсинге: {e}")
+        continue
+
+driver.quit()
+
+with open("svet.csv", 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file, delimiter=';')
+    writer.writerow(['Название', 'цена', 'ссылка'])
+    writer.writerows(parsed_data)
+
 
